cd945ffb797ea12855d3e307dfb5fde59c9d8873
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-07-07 13:53:05 +0300

    removed bad

--- src/polyfills.ts
+++
@@ -1,7 +0,0 @@
-
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-import '@webcomponents/custom-elements/src/native-shim';
-import '@webcomponents/custom-elements/custom-elements.min';
-
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
